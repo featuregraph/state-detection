7e57578c2d0839648703021b69cfb11f3b774cf3
Nazia.Habib <nazia.habib@patenergy.com>
2026-07-07 15:41:03 -0500

    wave pipeline

--- visual_notes.py
+++ visual_notes.py
@@ -5,52 +5,23 @@ import random
 import matplotlib
 import matplotlib.pyplot as plt
 import torch
+import seaborn as sns
 
-df = pd.read_csv('TEP_FaultFree_Training.csv')
+from lib.common.plotting.timeseries import plot_horizontal as plot
+from state_detection.rename_map import rename_map
+from state_detection.wave_pipeline import run_wave_pipeline
 
+df = pd.read_csv("TEP_Faulty_Training.csv")
 df = df.rename(columns=rename_map)
 
-cols = [
-    "reactor_pressure",
-    "reactor_level",
-    "reactor_temperature",
-#     "separator_pressure",
-#     "separator_level",
-#     "separator_temperature",
-#     "stripper_pressure",
-#     "stripper_level",
-#     "stripper_temperature",
-#     "compressor_work",
-    "reactor_cooling_water_valve",
-#     "condenser_cooling_water_valve",
-]
+run_summary = run_wave_pipeline(
+    df,
+    signals=["reactor_pressure", "reactor_temperature"],
+    group=["fault_number", "simulation_run"],
+    base_signal="reactor_pressure",
+    smooth_window=20,
+    diff_lag=10,
+    eps=0,
+)
 
-
-def smooth_signal(signal, group, window_size):
-    df[f'{signal}_smooth'] = df.groupby(group)[signal].rolling(window_size).mean().reset_index(level=0, drop=True)
-
-
-def enter(signal):
-    df[f'enter_{signal}'] = df[signal].astype(int).diff() == 1
-
-
-def exit(signal):
-    df[f'enter_{signal}'] = df[signal].astype(int).diff() == -1
-
-
-def state_num(signal):
-    df[f'{signal}_num'] = df[f'enter_{signal}'].cumsum()
-
-
-def measure(signal, group, measure, label):
-    df[f'{signal}_{label}'] = df.groupby(group).transform(measure)[signal]
-
-
-smooth_signal('reactor_pressure', 'simulation_run', 20)
-df['reactor_pressure_rising'] = df['reactor_pressure_smooth'].diff(10) > 0
-df['reactor_pressure_falling'] = df['reactor_pressure_smooth'].diff(10) < 0
-enter('reactor_pressure_rising')
-state_num('reactor_pressure_rising')
-measure('reactor_pressure_rising', 'reactor_pressure_rising_num', 'max', 'peak')
-measure('reactor_pressure_rising', 'reactor_pressure_rising_num', 'min', 'trough')
-# df['reactor_pressure_amplitude'] = (df['reactor_pressure_rising_peak'] - df['reactor_pressure_rising_trough']) / 2
+run_summary.to_csv('wave_summary_simulation_run_faults.csv')
